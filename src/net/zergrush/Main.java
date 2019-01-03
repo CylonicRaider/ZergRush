9e23ab2c70e2cdafe3949d2416512bb8dde20e59
CylonicRaider <CylonicRaider@users.noreply.github.com>
2019-01-03 14:31:12 +0100

    Perform GUI setup on Swing EDT

--- src/net/zergrush/Main.java
+++ src/net/zergrush/Main.java
@@ -1,16 +1,21 @@
 package net.zergrush;
 
+import javax.swing.SwingUtilities;
 import net.zergrush.ui.MainUI;
 
 public class Main {
 
     public static void main(String[] args) {
-        MainUI ui = new MainUI();
-        Game game = new Game(ui);
-        ui.setMessage("ZERG RUSH", "They are coming...");
-        MainUI.createWindow(ui).setVisible(true);
-        MainUI.scheduleRepeatedly(game.getUpdateRunnable(),
-                                  Game.UPDATE_INTERVAL);
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                MainUI ui = new MainUI();
+                Game game = new Game(ui);
+                ui.setMessage("ZERG RUSH", "They are coming...");
+                MainUI.createWindow(ui).setVisible(true);
+                MainUI.scheduleRepeatedly(game.getUpdateRunnable(),
+                                      Game.UPDATE_INTERVAL);
+            }
+        });
     }
 
 }
