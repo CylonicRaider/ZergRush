fdcd8d7d61a88aab5fb5b693c332648058d00d8a
CylonicRaider <CylonicRaider@users.noreply.github.com>
2019-01-03 23:32:45 +0100

    Implement player sprite

--- src/net/zergrush/Game.java
+++ src/net/zergrush/Game.java
@@ -1,6 +1,8 @@
 package net.zergrush;
 
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import net.zergrush.sprites.Player;
 import net.zergrush.sprites.Zerg;
 
 public class Game {
@@ -8,10 +10,12 @@ public class Game {
     public static final int UPDATE_INTERVAL = 16;
 
     private final GameUI ui;
+    private Player player;
     private Zerg demoZerg;
 
     public Game(GameUI ui) {
         this.ui = ui;
+        this.player = new Player(this);
         this.demoZerg = new Zerg(this);
         ui.setGame(this);
     }
@@ -21,18 +25,28 @@ public class Game {
     }
 
     public void update() {
-        if (demoZerg == null) return;
-        ui.markDamaged(demoZerg.getBounds());
-        if (! demoZerg.update()) {
-            demoZerg = null;
-        } else {
+        if (player != null) {
+            ui.markDamaged(player.getBounds());
+            if (! player.update()) {
+                player = null;
+            } else {
+                ui.markDamaged(player.getBounds());
+            }
+        }
+        if (demoZerg != null) {
             ui.markDamaged(demoZerg.getBounds());
+            if (! demoZerg.update()) {
+                demoZerg = null;
+            } else {
+                ui.markDamaged(demoZerg.getBounds());
+            }
         }
         ui.update();
     }
 
     public void draw(Graphics2D g) {
         if (demoZerg != null) demoZerg.draw(g);
+        if (player != null) player.draw(g);
     }
 
     public Runnable getUpdateRunnable() {
---
+++ src/net/zergrush/sprites/Player.java
@@ -0,0 +1,85 @@
+package net.zergrush.sprites;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Path2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.event.KeyEvent;
+import net.zergrush.Game;
+
+public class Player extends Sprite {
+
+    public enum Orientation { RIGHT, DOWN, LEFT, UP }
+
+    public static final double SIZE = 0.1;
+    public static final double SPEED = 0.03;
+    public static final double BOUND = 0.8;
+    public static final Color COLOR = new Color(0x008000);
+
+    public static final Shape SHAPE;
+
+    static {
+        Path2D shape = new Path2D.Double();
+        shape.moveTo(SIZE / 2, 0);
+        shape.lineTo(-SIZE / 2, SIZE / 2);
+        shape.lineTo(-SIZE / 2, -SIZE / 2);
+        shape.closePath();
+        SHAPE = shape;
+    }
+
+    private Orientation rot;
+
+    public Player(Game game) {
+        super(game);
+        rot = Orientation.RIGHT;
+        baseBounds.setRect(-SIZE / 2, -SIZE / 2, SIZE, SIZE);
+    }
+
+    public void move(Orientation dir) {
+        if (dir != null) rot = dir;
+        switch (rot) {
+            case RIGHT:
+                position.x += SPEED;
+                if (position.x > BOUND) position.x = BOUND;
+                break;
+            case DOWN:
+                position.y += SPEED;
+                if (position.y > BOUND) position.y = BOUND;
+                break;
+            case LEFT:
+                position.x -= SPEED;
+                if (position.x < -BOUND) position.x = -BOUND;
+                break;
+            case UP:
+                position.y -= SPEED;
+                if (position.y < -BOUND) position.y = -BOUND;
+                break;
+        }
+    }
+
+    public void draw(Graphics2D g) {
+        g.setColor(COLOR);
+        AffineTransform tr = g.getTransform();
+        g.translate(position.getX(), position.getY());
+        g.rotate(Math.PI / 2 * rot.ordinal());
+        g.fill(SHAPE);
+        g.setTransform(tr);
+    }
+
+    public boolean update() {
+        if (isKeyPressed(KeyEvent.VK_UP)) {
+            move(Orientation.UP);
+        } else if (isKeyPressed(KeyEvent.VK_DOWN)) {
+            move(Orientation.DOWN);
+        } else if (isKeyPressed(KeyEvent.VK_LEFT)) {
+            move(Orientation.LEFT);
+        } else if (isKeyPressed(KeyEvent.VK_RIGHT)) {
+            move(Orientation.RIGHT);
+        }
+        return true;
+    }
+
+}
