4760146d40bb274eb8042c78af6eaf0817ea007a
CylonicRaider <CylonicRaider@users.noreply.github.com>
2021-02-10 22:45:52 +0100

    Add window close link to help text

--- src/net/zergrush/ui/HTMLDialog.java
+++ src/net/zergrush/ui/HTMLDialog.java
@@ -7,16 +7,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.net.URL;
+import java.util.Map;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.KeyStroke;
+import javax.swing.event.HyperlinkEvent;
 
 public class HTMLDialog extends JDialog
-        implements HTMLPane.TitleChangeListener {
+        implements HTMLPane.TitleChangeListener, HTMLPane.PageActionListener {
 
     private static final long serialVersionUID = -4460891275078197826L;
 
     private final HTMLPane display;
+    private HTMLPane.PageActionListener listener;
 
     public HTMLDialog(Dialog owner) {
         super(owner);
@@ -32,6 +35,7 @@ public class HTMLDialog extends JDialog
         display = new HTMLPane();
         add(display);
         display.setTitleChangeListener(this);
+        display.setPageActionListener(this);
         closeOnEscape(this);
     }
 
@@ -39,8 +43,30 @@ public class HTMLDialog extends JDialog
         return display;
     }
 
+    public HTMLPane.PageActionListener getPageActionListener() {
+        return listener;
+    }
+
+    public void setPageActionListener(HTMLPane.PageActionListener l) {
+        listener = l;
+    }
+
     public void titleChanged(HTMLPane pane, String newTitle) {
         setTitle(newTitle);
+    }
+
+    public boolean pageActionInvoked(HTMLPane pane, HyperlinkEvent evt,
+            String url, Map<String, String> formData) {
+        if (url.startsWith("window:")) {
+            if (url.equals("window:close")) {
+                MainUI.closeWindow(this);
+            }
+            return true;
+        }
+        if (listener != null) {
+            return listener.pageActionInvoked(pane, evt, url, formData);
+        }
+        return false;
     }
 
     public void loadPage(URL url) {
